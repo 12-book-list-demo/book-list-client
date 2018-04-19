b849b202e4c7ba0f20f345cc617c00ac0e8d3d22
Khoa Huynh <khoawin92@gmail.com>
2018-04-18 19:20:32 -0700

    added URLs to ENV

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -5,8 +5,9 @@ var app = app || {};
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'insert cloud API server URL here';
-ENV.developmentApiUrl = 'insert local API server URL here';
+ENV.productionApiUrl = 'https://kh-dj-booklist.herokuapp.com';
+ENV.developmentApiUrl = 'http://localhost:3000';
+
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
 (function(module) {
